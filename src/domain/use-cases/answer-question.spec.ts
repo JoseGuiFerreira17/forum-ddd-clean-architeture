cfd015219e0a68151cf665d89a3d14aff331797f
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-01-31 15:56:07 -0300

    first commit

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,24 @@
+import { expect, test } from "vitest";
+import { AnswerQuestion } from "./answer-question";
+import { AnswerRepository } from "../repositories/answers-repository";
+import { Answer } from "../entities/answer";
+
+const mockAnswerRepository: AnswerRepository = {
+  create: async function (answer: Answer) {
+    return;
+  },
+};
+
+test("create answer", async () => {
+  const answerQuestion = new AnswerQuestion(mockAnswerRepository);
+
+  const answer = await answerQuestion.execute({
+    instructorId: "1",
+    questionId: "1",
+    content: "answer content",
+  });
+
+  expect(answer).toMatchObject({
+    content: "answer content",
+  });
+});

45b2def8a3ef172d9f91144defc89de7c91dec51
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-02-04 16:48:38 -0300

    creation of attachment entity

---
+++ src/domain/forum/enterprise/entities/attachment.ts
@@ -0,0 +1,27 @@
+import { Entity } from '@/core/entities/entity';
+import { UniqueEntityId } from '@/core/entities/unique-entity-id';
+
+export interface AttachmentProps {
+  title: string;
+  link: string;
+}
+
+export class Attachment extends Entity<AttachmentProps> {
+  get title() {
+    return this.props.title;
+  }
+
+  get link() {
+    return this.props.link;
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+  }
+
+  static create(props: AttachmentProps, id?: UniqueEntityId) {
+    const attachment = new Attachment(props, id);
+
+    return attachment;
+  }
+}

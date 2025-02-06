6c27ef8302d5b778c01e217781871b38a6b7451d
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-02-06 16:11:27 -0300

    creation of functionality to define best response

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -5,6 +5,7 @@ import dayjs from 'dayjs';
 import { AggregateRoot } from '@/core/entities/aggregate-root';
 import { QuestionAttachment } from './question-attachment';
 import { QuestionAttachmentList } from './question-attachment-list';
+import { QuestionBestAnswerChosenEvent } from '../events/question-best-answer-chosen-event';
 
 export interface QuestionProps {
   authorId: UniqueEntityId;
@@ -64,6 +65,12 @@ export class Question extends AggregateRoot<QuestionProps> {
   }
 
   set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    if (bestAnswerId && bestAnswerId !== this.props.bestAnswerId) {
+      this.addDomainEvent(
+        new QuestionBestAnswerChosenEvent(this, bestAnswerId),
+      );
+    }
+
     this.props.bestAnswerId = bestAnswerId;
     this.touch();
   }
